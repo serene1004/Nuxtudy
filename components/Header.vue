6039c224a08315f5712be8f6f340748a9eff5a08
serene1004 <vcbxnzn@naver.com>
2025-10-10 23:03:49 +0900

    Nuxt 초기 설정

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header>
+    <h1>Nuxtudy</h1>
+    <nav>
+      <NuxtLink to="/">Home</NuxtLink> |
+      <NuxtLink to="/about">About</NuxtLink>
+    </nav>
+  </header>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <Header />
+  <main>
+    <slot />
+  </main>
+</template>
+
+<script setup>
+import Header from '~/components/Header.vue'
+</script>
