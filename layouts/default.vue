b5d12df7f0a00576b302bed2a60ea0f3bc5101f7
serene1004 <vcbxnzn@naver.com>
2025-10-12 02:01:53 +0900

    feat: 타이틀 설정

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <template>
   <Header />
   <main class="mx-auto max-w-[1440px] p-4 sm:px-6 lg:px-8">
-    <slot />
+    <div class="flex gap-4">
+      <div class="flex-1 min-w-0">
+        <slot />
+      </div>
+
+      <WeatherNow />
+    </div>
   </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import Header from '~/components/Header.vue'
+
+const appConfig = useAppConfig()
+
+useHead({
+  title: appConfig.title
+})
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
 <template>
-  <div class="flex gap-4">
-    <div class="flex-1 min-w-0">
-      <h2 class="text-lg font-semibold text-neutral-900 dark:text-neutral-100">Welcome Home</h2>
-    </div>
-
-    <div class="w-full max-w-[192px] shrink-0">
-      <WeatherNow />
-    </div>
-  </div>
+  <h2 class="text-lg text-neutral-700 dark:text-neutral-100">Welcome Home</h2>
 </template>
